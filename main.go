428dfe402c656685bb4fc1597937cd36ab419cca
Jon Wear <jon@dramafever.com>
2014-10-07 11:31:34 -0400

    added error typing example

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"os"
 
 	"code.google.com/p/gcfg"
 )
@@ -11,17 +11,22 @@ import (
 var configPath = flag.String("class", "fighter", "Value of class to build.  Default is: fighter")
 
 //You can put the flag definitions in the init function as well.
-//Nothings gets called until you call flag.Parse()
+//Flag values are pulled in until you call flag.Parse()
 
 func main() {
 	var cfg Config
 	flag.Parse()
 	configpath := fmt.Sprintf("config/%v.gcfg", *configPath)
 	if err := gcfg.ReadFileInto(&cfg, configpath); err != nil {
-		log.Println("Error: ", err)
+		if pathErr, ok := err.(*os.PathError); ok {
+			fmt.Printf("PathError: %v\n", pathErr)
+			fmt.Printf("Op: %v\nPath: %v\n", pathErr.Op, pathErr.Path)
+		} else {
+			fmt.Printf("Error Type: %T", err)
+			fmt.Println("Error: ", err)
+		}
 	} else {
 		character := NewCharacter(cfg)
 		fmt.Printf("Character Info...\n%v\n", character)
 	}
-
 }
